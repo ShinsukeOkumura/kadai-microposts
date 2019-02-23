7f1ec53b09b5a57c277f82fec9bffa1dff30c6d1
shinsuke.okumura <shinryo.1707@gmail.com>
2019-02-23 13:15:13 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 	
-	include SessionHelper
+	include SessionsHelper
 	
 	private
 	
 	def require_user_logged_in
 		unless logged_in?
+			p 'login_url = ', login_url
 			redirect_to login_url
 		end
+	end
+	
+	def counts(user)
+		@count_microposts = user.microposts.count
+		p '@count_microposts = ', @count_microposts
 	end
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,10 @@
 class ToppagesController < ApplicationController
   def index
+		if logged_in?
+			# form_for用
+			@micropost = current_user.microposts.build
+			p @micropost
+			@microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+		end
   end
 end
